2a680519b79a8596d56d528adcbb9fd7c6b5bf56
ygz <ygzaydns@gmail.com>
2020-04-28 14:10:23 +0300

    first touch

--- src/App.js
+++ src/App.js
@@ -7,27 +7,30 @@ class App extends Component {
     super();
 
     this.state = {
-      string: 'Hello MF'
+      monsters : [
+        {
+          name: 'Frankenstein',
+          id: 'asc1'
+        },
+        {
+          name: 'Dracula',
+          id: 'asc2'
+        },
+        {
+          name: 'Zombie',
+          id: 'asc3'
+        }
+      ]
     };
   }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            {this.state.string}
-          </p>
-          <button onClick={() => this.setState({ string: 'Hello FM' })}>Change Text</button>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {
+          this.state.monsters.map(monster => 
+          <h1 key={monster.id}>{ monster.name }</h1>)
+        }
       </div>
     );
   }
