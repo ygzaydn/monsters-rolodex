f0acb52b11ec6b288d231d6538df1217290b5e7a
ygz <ygzaydns@gmail.com>
2020-04-29 11:54:50 +0300

    first touch

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -4,7 +4,7 @@ import { Card } from '../card/card.component';
 import './card-list.styles.css'
 
 const CardList = (props) => {
-    console.log(props);
+    
     return (
         <div className='card-list'>
         {props.monsters.map(monster => (
