63b113affca45620d13d50ab00a935f864b27577
ygz <ygzaydns@gmail.com>
2020-04-30 09:17:02 +0300

    Updates

---
+++ precache-manifest.094d13c70e350e904ec11a2ae4a8d88e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab4a77f0d69733cc7c5cc10b6ed172d6",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "dbedb2905f319d8a667c",
+    "url": "/monsters-rolodex/static/css/main.90de9fd1.chunk.css"
+  },
+  {
+    "revision": "9f834d1fc4df12fb31cd",
+    "url": "/monsters-rolodex/static/js/2.308474c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.308474c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbedb2905f319d8a667c",
+    "url": "/monsters-rolodex/static/js/main.b8867414.chunk.js"
+  },
+  {
+    "revision": "0c81cb278443448182f0",
+    "url": "/monsters-rolodex/static/js/runtime-main.165419b0.js"
+  }
+]);
